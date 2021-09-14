17b1445759215165a07c26277bcfd82199ad88f5
MarkNauf <mnaufel@asu.edu>
2021-09-14 03:41:05 -0700

    Work with web

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-import { Platform } from "react-native";
-import LinearGradientIos from "./index.ios.js";
-import LinearGradientAndroid from "./index.android.js";
-import LinearGradientWindows from "./index.windows.js";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View } from 'react-native';
 
-const LinearGradient = Platform.OS === "ios"
-  ? LinearGradientIos : Platform.OS === "android"
-  ? LinearGradientAndroid : LinearGradientWindows;
+export default class LinearGradient extends Component {
 
-export default LinearGradient;
+  static propTypes = {
+    start: PropTypes.shape({ x: PropTypes.number, y: PropTypes.number }).isRequired,
+    end: PropTypes.shape({ x: PropTypes.number, y: PropTypes.number }).isRequired,
+    locations: PropTypes.arrayOf(PropTypes.number),
+    colors: PropTypes.arrayOf(PropTypes.string).isRequired,
+    children: PropTypes.oneOfType([PropTypes.node, PropTypes.arrayOf(PropTypes.node)]),
+  };
+
+  render() {
+    const { start, end, locations, colors, style, children, ...otherProps } = this.props;
+    const vec = { x: end.x - start.x, y: -(end.y - start.y) };
+    const angleRad = Math.atan(vec.y / vec.x);
+    const angleDeg = Math.round((angleRad * 180) / Math.PI);
+    const angleWeb = -angleDeg + 90;
+    const realLocations = locations || colors.map((color, i) => (1 / (colors.length - 1)) * i);
+    const colorStrings = colors.map((color, i) => `${color} ${Math.round(realLocations[i] * 100)}%`).join(', ');
+    return (
+      <View
+        {...otherProps}
+        style={[style, { backgroundImage: `linear-gradient(${angleWeb}deg, ${colorStrings})` }]}>
+        {children}
+      </View>
+    );
+  }
+}
